8998da677a2ab3282249a1d023854f8e6d12f3c4
thangqn <namthang.bkhn@gmail.com>
2022-03-24 23:39:32 +0700

    add Category

---
+++ BulkyBook/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDatetime { get; set; } = DateTime.Now;
+    }
+}
